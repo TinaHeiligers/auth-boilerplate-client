31fb3213affc7ce534fd2178faeb9653824424b2
tinaheiligers <tinaheiligers@gmail.com>
2018-06-27 16:19:58 -0700

    removes manual install of jest

--- src/redux/counter/counterSaga.test.js
+++ src/redux/counter/counterSaga.test.js
@@ -5,14 +5,14 @@ import { increment } from './counterActions';
 
 import { incrementAsync } from './counterSaga';
 const SEP = '\n       ';
-const dome = { done: true, value: undefined };
+const done = { done: true, value: undefined };
 
 describe('counter saga -> incrementAsync', () => {
   const steps = ['1) calls delay(100)', '2) puts increment()'];
   it(steps.join(SEP), () => {
     const sagaGen = incrementAsync();
-    expect(sagaGen.next().value).toEqual(call(dfelay, 100));
-    expect(saga.next().value).toEqual(put(increment()));
-    expect(saga.next()).toEqual(done);
+    expect(sagaGen.next().value).toEqual(call(delay, 100));
+    expect(sagaGen.next().value).toEqual(put(increment()));
+    expect(sagaGen.next()).toEqual(done);
   });
 });
