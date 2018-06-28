22d1d668e6eadfd02b28eb3b1573b6a767c87e4a
tinaheiligers <tinaheiligers@gmail.com>
2018-06-28 08:06:14 -0700

    Begins with login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-// TODO convert to styled components
+// These styled items can be moved to another folder, extended and shared.
 const AppMainDiv = styled.div`
   text-align: center;
+  font-family: Tahoma;
+  font-size: large;
 `;
 const AppHeader = styled.header`
   background-color: #30bdf8;
-  height: 150px;
-  padding: 20px;
+  height: 20vh;
+  padding: 2vh;
   color: white;
 `;
 const AppDiv = styled.div`
@@ -22,7 +24,21 @@ const AppH1 = styled.h1`
 const AppP = styled.p`
   font-size: large;
 `
+const LoginButton = styled.button`
+  display: block;
+  border-color: black;
+  width: 14vw;
+  height: 5vh;
+  text-align: center;
+  margin: 2vh auto 2vh;
+  border-radius: 10px;
+`
+
 class App extends Component {
+  login = (e) => {
+    e.preventDefault();
+    console.log(`Login Button Clicked! (e):`, e)
+  }
   render() {
     return (
       <AppMainDiv>
@@ -33,6 +49,10 @@ class App extends Component {
         <AppP>
           To get started, edit <code>src/App.js</code> and save to reload.
         </AppP>
+        <div>
+          Log in to see more
+          <LoginButton type='LoginButton' onClick={this.login}>Log In</LoginButton>
+        </div>
       </AppMainDiv>
     );
   }
