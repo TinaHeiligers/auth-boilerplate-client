88fa6f248a26090f7fd5b76b23515d19cf376762
tinaheiligers <tinaheiligers@gmail.com>
2018-06-27 17:14:54 -0700

    Converts App to a styled React Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
+// TODO convert to styled components
+const AppMainDiv = styled.div`
+  text-align: center;
+`;
+const AppHeader = styled.header`
+  background-color: #30bdf8;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+const AppDiv = styled.div`
+  height: 80px;
+  color: white;
+  font-size: 4em;
+`
+const AppH1 = styled.h1`
+  font-size: 1.5em;
+`
+const AppP = styled.p`
+  font-size: large;
+`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-logo">TALLWAVE</div>
-          <h1 className="App-title">Welcome to React for Auth!</h1>
-        </header>
-        <p className="App-intro">
+      <AppMainDiv>
+        <AppHeader>
+          <AppDiv>TALLWAVE</AppDiv>
+          <AppH1>Welcome to React for Auth!</AppH1>
+        </AppHeader>
+        <AppP>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </AppP>
+      </AppMainDiv>
     );
   }
 }
